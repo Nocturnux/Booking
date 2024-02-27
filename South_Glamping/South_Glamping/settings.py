8d7f40d39460d623db5754555ba6c093677a0781
Nocturnux <genjuro128@yahoo.es>
2024-02-27 17:30:20 -0500

    UpdateModel

--- South_Glamping/South_Glamping/settings.py
+++ South_Glamping/South_Glamping/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'payment',
     'booking_service',
     'booking_cabin',
-    'user',
 ]
 
 MIDDLEWARE = [
--- South_Glamping/customer/models.py
+++ South_Glamping/customer/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=255)
     document = models.IntegerField(unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     cellphone = models.IntegerField()
     status = models.BooleanField(default=True)
 
