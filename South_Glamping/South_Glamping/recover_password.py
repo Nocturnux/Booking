c4e43baef916878e8d5eddaa023a527e78d325c4
SergioEsmeral2004 <yeyoesmeral@hotmail.com>
2024-03-10 18:10:51 -0500

    recover password, cambio a ingles y detalles

--- South_Glamping/South_Glamping/recover_password.py
+++ South_Glamping/South_Glamping/recover_password.py
@@ -1,44 +1,50 @@
-
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import random
 import string
 
-def generar_contraseña():
-    caracteres = string.ascii_letters + string.digits
-    longitud = 10
-    return ''.join(random.choice(caracteres) for i in range(longitud))
+def generate_password():
+    characters = string.ascii_letters + string.digits
+    length = 10
+    return ''.join(random.choice(characters) for i in range(length))
 
-def enviar_correo(destinatario, contraseña):
+def send_email(addressee, password):
     # Configuración del servidor SMTP
     smtp_server = 'smtp.gmail.com'
-    puerto = 587
-    remitente = 'TU CORREO'
-    contraseña_smtp = 'TU CLAVE'
+    port = 587
+    sender = 'southglamping.sg@gmail.com'
+    password_smtp = 'cwne kdqi pzpn xoln'
 
     # Crear el mensaje
-    mensaje = MIMEMultipart()
-    mensaje['From'] = remitente
-    mensaje['To'] = destinatario
-    mensaje['Subject'] = 'Recuperación de contraseña'
+    message = MIMEMultipart()
+    message['From'] = sender #remitente
+    message['To'] = addressee #destinatario
+    message['Subject'] = 'Recuperación de contraseña'
 
-    cuerpo = f'Tu nueva contraseña es: {contraseña}'
-    mensaje.attach(MIMEText(cuerpo, 'plain'))
+    body = f'Tu nueva contraseña es: {password}'
+    message.attach(MIMEText(body, 'plain', 'utf-8'))
 
     # Iniciar sesión en el servidor SMTP
-    servidor = smtplib.SMTP(smtp_server, puerto)
-    servidor.starttls()
-    servidor.login(remitente, contraseña_smtp)
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls()
+    server.login(sender, password_smtp)
 
     # Enviar el correo electrónico
-    servidor.send_message(mensaje)
+    server.send_message(message)
 
     # Cerrar la conexión
-    servidor.quit()
+    server.quit()
 
 # Función principal
-def recuperar_contraseña(email):
-    correo_destino = email
-    nueva_contraseña = generar_contraseña()
-    enviar_correo(correo_destino, nueva_contraseña)
+def recover_password(email):
+    destination_email= email
+    new_password= generate_password()
+    send_email(destination_email, new_password)
+
+def recover_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        """ Cosultar el usuario por el correo  y cambiar la contraseña encriptada"""
+        recover_password(email)
+        return render (request, 'recover_password.html', {'email': email})
