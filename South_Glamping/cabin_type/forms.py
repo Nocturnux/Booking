4d3d98d04ae813e2e8e07032f9d5b7555c1c20fe
Camilaaj18 <mariac_alvarezj@soy.sena.edu.co>
2024-03-13 23:08:28 -0500

    Update landing, cabin_type

--- South_Glamping/cabin_type/forms.py
+++ South_Glamping/cabin_type/forms.py
@@ -7,10 +7,14 @@ class Cabin_typeForm(forms.ModelForm):
         fields = "__all__"
         exclude = ['status']
         labels = {
+            'image': 'Imagen',
             'name': 'Nombre',
+            'description': 'Descripción',
             'status': 'Estado',                    
         }
         widgets = {
+            'image': forms.FileInput(attrs={'placeholder': 'Ingrese la imagen del tipo de cabaña'}),
             'name': forms.TextInput(attrs={'placeholder': 'Ingresa el nombre'}),
+            'description': forms.TextInput(attrs={'placeholder': 'Ingresa la descripción'}),
             'status': forms.TextInput(attrs={'placeholder': 'Ingresa el Estado'}),           
         }
